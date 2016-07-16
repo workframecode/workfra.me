b497290eb8e41accfd7b5c12167fb5f6953196d1
Arvind Kumar <arvind@enkrypt.in>
2016-07-16 17:01:30 +0530

    Made source up to date.

---
+++ html/js/pagefunctionality.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+	$(window).bind('scroll', function(){
+		if ($(window).scrollTop() > 50){
+			if (!$('.navbar').hasClass('navdown'))
+				swapbar(true);
+		}
+		else{
+			if ($('.navbar').hasClass('navdown'))
+				swapbar(false);
+		}
+	});
+});
+
+var swapbar=function(fromdefault){
+	$('.navbar').slideUp(200, function(){
+		if (fromdefault){
+			$('.navbar').addClass('navdown');
+			$('.navbar-brand').html('<img src="images/banner-monochrome-black.png" height="20" width="100">');
+		}
+		else{
+			$('.navbar').removeClass('navdown');
+			$('.navbar-brand').html('<img src="images/banner-monochrome-white.png" height="20" width="100">');
+		}
+		$('.navbar').slideDown(200);
+	});
+}
+
+var captchapass=function(){
+	
+}
+
+var captchafail=function(){
+	
+}
