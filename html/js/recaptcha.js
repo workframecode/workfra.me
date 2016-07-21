fee489479e4dfc265ac94209f9625c3abc183ffe
Arvind Kumar <arvind@enkrypt.in>
2016-07-22 02:24:02 +0530

    Displaying errors propogated from recaptcha backend.

--- html/js/recaptcha.js
+++ html/js/recaptcha.js
@@ -4,9 +4,34 @@ var recaptchaResponse=function(response){
 			console.log(response);
 			console.log(result);
 			if (result.success){
+				$('#formpass').fadeIn();
+				$('.form-control').each(function(){
+					$(this).val('');
+				});
+				setTimeout(function(){
+					$('#formpass').fadeOut();
+				},3000);
 				console.log('pass');
 			}
 			else{
+				if (result.errormsg){
+					if (result.nofix){
+						$('#formerror').html('Something went wrong. If you know of an alternate way to reach the developers, tell them about this error. You can also check the console log for more details.');
+						console.log('Backend error while submitting contact form: '+result.errormsg)
+						$('#formerror').fadeIn();
+						grecaptcha.reset();
+					}
+					else{
+						$('#formerror').html(result.errormsg);
+						$('#formerror').fadeIn();
+						grecaptcha.reset();
+					}
+				}
+				else{
+					$('#formerror').html('You did not answer the captcha right. Try again.');
+					$('#formerror').fadeIn();
+					grecaptcha.reset();
+				}
 				console.log('fail');
 			}
 		});
