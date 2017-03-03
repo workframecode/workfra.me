6fab3bd6967b4bc83a09052abc81039eb9f6fb6d
Arvind Kumar <arvind@enkrypt.in>
2017-03-04 04:49:07 +0530

    Refactored validation elements from id to class

--- public/js/recaptcha.js
+++ public/js/recaptcha.js
@@ -6,43 +6,43 @@ var recaptchaResponse=function(cid) {
 				$.post('/contactformrecaptcha', {response: response, name: $("#name").val(), email: $("#email").val(), college: $("#college").val(), message: $("#message").val()}, function(result){
 					$('#contactform .loader').css('display', 'none');
 					if (result && result.success){
-						$('#contactform #formpass').fadeIn().css('display','inline-block');
+						$('#contactform .formpass').fadeIn().css('display','inline-block');
 						$('html, body').stop().animate({
-							scrollTop: $('#contactform #formpass').offset().top-$('.navbar').height()-20
+							scrollTop: $('#contactform .formpass').offset().top-$('.navbar').height()-20
 						}, 700);
 						$('#contactform .form-control').each(function(){
 							$(this).val('');
 						});
 						setTimeout(function(){
-							$('#contactform #formpass').fadeOut();
+							$('#contactform .formpass').fadeOut();
 						},3000);
 						grecaptcha.reset();
 					}
 					else{
 						if (result.errormsg){
 							if (result.nofix){
-								$('#contactform #formerror').html('Something went wrong. If you know of an alternate way to reach the developers, tell them about this error. You can also check the console log for more details.');
+								$('#contactform .formerror').html('Something went wrong. If you know of an alternate way to reach the developers, tell them about this error. You can also check the console log for more details.');
 								console.log('Backend error while submitting contact form: '+result.errormsg);
-								$('#contactform #formerror').fadeIn().css('display','inline-block');
+								$('#contactform .formerror').fadeIn().css('display','inline-block');
 								$('html, body').stop().animate({
-									scrollTop: $('#contactform #formerror').offset().top-$('.navbar').height()-20
+									scrollTop: $('#contactform .formerror').offset().top-$('.navbar').height()-20
 								}, 700);
 								grecaptcha.reset();
 							}
 							else{
-								$('#contactform #formerror').html(result.errormsg);
-								$('#contactform #formerror').fadeIn().css('display','inline-block');
+								$('#contactform .formerror').html(result.errormsg);
+								$('#contactform .formerror').fadeIn().css('display','inline-block');
 								$('html, body').stop().animate({
-									scrollTop: $('#contactform #formerror').offset().top-$('.navbar').height()-20
+									scrollTop: $('#contactform .formerror').offset().top-$('.navbar').height()-20
 								}, 700);
 								grecaptcha.reset();
 							}
 						}
 						else{
-							$('#contactform #formerror').html('You did not answer the captcha right. Try again.');
-							$('#contactform #formerror').fadeIn().css('display','inline-block');
+							$('#contactform .formerror').html('You did not answer the captcha right. Try again.');
+							$('#contactform .formerror').fadeIn().css('display','inline-block');
 							$('html, body').stop().animate({
-								scrollTop: $('#contactform #formerror').offset().top-$('.navbar').height()-20
+								scrollTop: $('#contactform .formerror').offset().top-$('.navbar').height()-20
 							}, 700);
 							grecaptcha.reset();
 						}
@@ -50,10 +50,10 @@ var recaptchaResponse=function(cid) {
 				});
 			}
 			else{
-				$('#contactform #formerror').html('Your form has errors that need to be resolved.');
-				$('#contactform #formerror').fadeIn().css('display','inline-block');
+				$('#contactform .formerror').html('Your form has errors that need to be resolved.');
+				$('#contactform .formerror').fadeIn().css('display','inline-block');
 				$('html, body').stop().animate({
-					scrollTop: $('#contactform #formerror').offset().top-$('.navbar').height()-20
+					scrollTop: $('#contactform .formerror').offset().top-$('.navbar').height()-20
 				}, 700);
 				grecaptcha.reset();
 			}
@@ -65,7 +65,7 @@ var recaptchaResponse=function(cid) {
 			$.post('/slackinviterecaptcha', {response: response, email: $("#slackemail").val()}, function(result){
 				$('.slackinvitebox .loader').css('display', 'none');
 				if (result && result.success){
-					$('.slackinvitebox #formpass').fadeIn().css('display','inline-block');
+					$('.slackinvitebox .formpass').fadeIn().css('display','inline-block');
 					$('#slackemail').val('');
 					setTimeout(function(){
 						hideSlackInvite();
@@ -75,20 +75,20 @@ var recaptchaResponse=function(cid) {
 				else{
 					if (result.errormsg){
 						if (result.nofix){
-							$('#formerror').html('Something went wrong. If you know of an alternate way to reach the developers, tell them about this error. You can also check the console log for more details.');
+							$('.formerror').html('Something went wrong. If you know of an alternate way to reach the developers, tell them about this error. You can also check the console log for more details.');
 							console.log('Backend error while submitting slack invite request: '+result.errormsg);
-							$('#formerror').fadeIn().css('display','inline-block');
+							$('.formerror').fadeIn().css('display','inline-block');
 							grecaptcha.reset();
 						}
 						else{
-							$('#formerror').html(result.errormsg);
-							$('#formerror').fadeIn().css('display','inline-block');
+							$('.formerror').html(result.errormsg);
+							$('.formerror').fadeIn().css('display','inline-block');
 							grecaptcha.reset();
 						}
 					}
 					else{
-						$('#formerror').html('You did not answer the captcha right. Try again.');
-						$('#formerror').fadeIn().css('display','inline-block');
+						$('.formerror').html('You did not answer the captcha right. Try again.');
+						$('.formerror').fadeIn().css('display','inline-block');
 						grecaptcha.reset();
 					}
 				}
